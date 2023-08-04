4ae4793c1a49de4b308fa784916c471a44b4cb7e
a <a>
2023-08-04 16:44:57 +0900

    Initial Commit

---
+++ sample8/sample8App.swift
@@ -0,0 +1,17 @@
+//
+//  sample8App.swift
+//  sample8
+//
+//  Created by 安保　快斗 on 2023/08/04.
+//
+
+import SwiftUI
+
+@main
+struct sample8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
